6f9864b3f6d69dcc1d1f1347647415c93b209aa6
Arsenii Petrov <arseniiptrv@gmail.com>
2019-12-13 18:35:17 +0200

    Created SkillActivity, enabled toggle buttons on LeagueActivity and passed data from LA to SA via intent

--- app/src/main/java/com/arseniiptrv/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/arseniiptrv/swoosh/LeagueActivity.kt
@@ -1,12 +1,57 @@
 package com.arseniiptrv.swoosh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        leagueNextBtn.setOnClickListener {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            if (selectedLeague != "") {
+                startActivity(skillIntent)
+            }
+            else {
+                Toast.makeText(this, "Please select a league", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
+    var selectedLeague = ""
+
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "Mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "Womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "Co-Ed"
+    }
+
+    /*fun skillBtnClicked (view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+    }*/
+
+    
 }
--- app/src/main/java/com/arseniiptrv/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/arseniiptrv/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.arseniiptrv.swoosh
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
