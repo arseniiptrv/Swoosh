57f84c05e5e2afaa33349f16f4be30f5e86cd0f7
Arsenii Petrov <arseniiptrv@gmail.com>
2019-12-16 23:49:56 +0200

    Overrided funs for saving and restoring instance state on LA and SA

--- app/src/main/java/com/arseniiptrv/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/arseniiptrv/swoosh/Controller/LeagueActivity.kt
@@ -14,6 +14,11 @@ class LeagueActivity : AppCompatActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -27,6 +32,13 @@ class LeagueActivity : AppCompatActivity() {
             else {
                 Toast.makeText(this, "Please select a league", Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
         }
     }
 
--- app/src/main/java/com/arseniiptrv/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/arseniiptrv/swoosh/Controller/SkillActivity.kt
@@ -14,6 +14,11 @@ class SkillActivity : AppCompatActivity() {
 
     lateinit var player: Player
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
@@ -24,6 +29,13 @@ class SkillActivity : AppCompatActivity() {
         //selectedLeague = stringExtra
         //Log.e("ddd", selectedLeague)
         //Log.i("league", selectedLeague)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+           player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
     fun onSkillFinishClicked(view: View) {
